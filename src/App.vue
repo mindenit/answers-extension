1e5cb16cd8183bca525b602938c9126939fd4d0a
perkinson1251 <tttrashutin@gmail.com>
2024-09-24 11:14:57 +0300

    feat: add basic copying functionlaity

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,37 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Button } from '@mindenit/ui'
+import { ref } from 'vue'
+
+const textContainer = ref('')
+
+const getSelectedText = async () => {
+  try {
+    const [tab] = await chrome.tabs.query({ active: true, currentWindow: true })
+
+    chrome.scripting.executeScript(
+      {
+        target: { tabId: tab.id! },
+        func: () => window.getSelection()?.toString() || ''
+      },
+      (injectionResults) => {
+        if (injectionResults && injectionResults.length > 0) {
+          textContainer.value =
+            injectionResults[0].result || 'No text selected.'
+        } else {
+          textContainer.value = 'No text selected.'
+        }
+      }
+    )
+  } catch (error) {
+    console.error('Error executing script:', error)
+    textContainer.value = 'An error occurred.'
+  }
+}
+</script>
 
 <template>
-  123
+  <div class="w-[300px]">
+    <Button @click="getSelectedText">Пошук</Button>
+    <div id="text-container">{{ textContainer }}</div>
+  </div>
 </template>

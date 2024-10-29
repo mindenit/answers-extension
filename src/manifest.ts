2bdb849e5fadd5b45939a4d84baff3c5ba4647b0
Volodymyr Nakonechnyi <volodymyr.nakonechnyi28@gmail.com>
2024-10-29 22:45:47 +0200

    chore: made popup better

--- src/manifest.ts
+++ src/manifest.ts
@@ -8,21 +8,15 @@ export default defineManifest({
   name: `${packageData.displayName || packageData.name}${isDev ? ` ➡️ Dev` : ''}`,
   description: packageData.description,
   version: packageData.version,
-  version_name: "0.0.1b3",
+  version_name: "0.0.1b4",
   manifest_version: 3,
   icons: {
-    16: 'img/logo-16.png',
-    32: 'img/logo-34.png',
-    48: 'img/logo-48.png',
     72: 'img/icon-72.png',
-    128: 'img/logo-128.png',
   },
   action: {
     default_popup: 'popup.html',
-    default_icon: 'img/logo-48.png',
+    default_icon: 'img/icon-72.png',
   },
-  options_page: 'options.html',
-  devtools_page: 'devtools.html',
   background: {
     service_worker: 'src/background/index.ts',
     type: 'module',
@@ -36,12 +30,9 @@ export default defineManifest({
       js: ['src/contentScript/index.tsx'],
     },
   ],
-  side_panel: {
-    default_path: 'sidepanel.html',
-  },
   web_accessible_resources: [
     {
-      resources: ['img/logo-16.png', 'img/logo-34.png', 'img/logo-48.png', 'img/icon-72.png', 'img/logo-128.png'],
+      resources: ['img/icon-72.png'],
       matches: [
         "*://*.dl.nure.ua/*",
         "*://*.answers.mindenit.org/*"

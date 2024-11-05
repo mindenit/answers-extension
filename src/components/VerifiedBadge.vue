4aa9b754866f36c9cd47a274c13e0d9ec68efcb8
Volodymyr Nakonechnyi <volodymyr.nakonechnyi28@gmail.com>
2024-11-05 21:04:38 +0200

    fix: resolving conflicts

--- src/components/VerifiedBadge.vue
+++
@@ -1,6 +0,0 @@
-<script lang="ts" setup>
-import { Badge } from '@mindenit/ui'
-</script>
-<template>
-  <Badge color="success">Верифіковано</Badge>
-</template>
--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,29 @@
-import vue from '@vitejs/plugin-vue'
-import { fileURLToPath, URL } from 'url'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
+import { crx } from '@crxjs/vite-plugin'
+import react from '@vitejs/plugin-react'
+
+import manifest from './src/manifest'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: [
-      {
-        find: '@',
-        replacement: fileURLToPath(new URL('./src', import.meta.url))
-      }
-    ]
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    define: {
+      'process.env.NODE_ENV': JSON.stringify(env.NODE_ENV),
+      'process.env.API_LLM': JSON.stringify(env.API_LLM),
+      'process.env.API_BACKEND': JSON.stringify(env.API_BACKEND),
+    },
+    build: {
+      emptyOutDir: true,
+      outDir: 'build',
+      rollupOptions: {
+        output: {
+          chunkFileNames: 'assets/chunk-[hash].js',
+        },
+      },
+    },
+
+    plugins: [crx({ manifest }), react()],
   }
 })
